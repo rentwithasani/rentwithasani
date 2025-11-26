11e9b27d8bc2341013926bffb8522231a7dcf93f
rentwithasani <rentwithasani@gmail.com>
2025-11-26 16:55:09 -0500

    main.jsx

--- main.jsx
+++ main.jsx
@@ -1,8 +1,7 @@
-// main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
-import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
