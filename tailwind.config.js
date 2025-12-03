a6af52e5e21c570106497abb3e9585ae4e87ad84
rentwithasani <rentwithasani@gmail.com>
2025-12-03 12:33:44 -0500

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 export default {
-  content: [
-    "./index.html",
-    "./**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+content: [
+  "./index.html",
+  "./src/**/*.{js,jsx,ts,tsx}",
+  "./App.jsx",
+  "./components/**/*.{js,jsx,ts,tsx}",
+];
